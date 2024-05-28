1939698a7aaf3055b6ccca9c5a11294527710228
nhatan22 <nhatan22@gmail.com>
2024-05-28 15:02:33 +0700

    Modify Model Role

--- DiamondShopSystem/src/main/java/com/example/DiamondShopSystem/model/Role.java
+++ DiamondShopSystem/src/main/java/com/example/DiamondShopSystem/model/Role.java
@@ -1,2 +1,28 @@
-package com.example.DiamondShopSystem.model;public class Role {
+package com.example.DiamondShopSystem.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="Role")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long roleId;
+    private String roleName;
+
+    public Long getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Long roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
 }
