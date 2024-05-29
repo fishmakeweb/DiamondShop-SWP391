892727232c68351ca3a76003302d47c3a7e68711
Nhat An <phungnhatan2204@gmail.com>
2024-05-29 18:04:29 +0700

    Filter DropBox for Jewelry Type,Gemstone

--- DiamondShopSystem/customer-client/src/App.js
+++ DiamondShopSystem/customer-client/src/App.js
@@ -2,19 +2,15 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Homepage from './pages/Homepage';
 import Jewelry from './pages/Jewelry';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
 
 function App() {
   return (
     <Router>
       <div>
-        <Navbar />
         <Routes>
           <Route path="/" element={<Homepage />} />
           <Route path="/jewelry" element={<Jewelry />} />
         </Routes>
-        <Footer />
       </div>
     </Router>
   );
