663030eb66554cd111db863ca55c2e9031df8868
Nhat An <phungnhatan2204@gmail.com>
2024-05-29 21:54:58 +0700

    Add JewelryService FindAllByCategoryId

--- DiamondShopSystem/src/main/java/com/example/DiamondShopSystem/service/JewelryService.java
+++ DiamondShopSystem/src/main/java/com/example/DiamondShopSystem/service/JewelryService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class JewelryService {
@@ -46,4 +47,11 @@ public class JewelryService {
     public void deleteJewelry(Long id) {
         jewelryRepository.deleteById(id);
     }
+    public List<Jewelry> findAllByCategoryId(Long categoryId) {
+        return jewelryRepository.findAll().stream()
+                .filter(jewelry -> jewelry.getCategory().getCategoryId().equals(categoryId))
+                .collect(Collectors.toList());
+    }
+
+
 }
