37a30dd7ba80a6d5d0106b6f7bb9dcf617a8ea52
Nhat An <phungnhatan2204@gmail.com>
2024-06-05 22:00:43 +0700

    Modify Category controller

---
+++ DiamondShopSystem/src/main/java/com/example/DiamondShopSystem/controller/CategoryController.java
@@ -0,0 +1,41 @@
+package com.example.DiamondShopSystem.controller;
+
+import com.example.DiamondShopSystem.model.Category;
+import com.example.DiamondShopSystem.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:3000")
+public class CategoryController {
+    @Autowired
+    private CategoryService categoryService;
+
+    @GetMapping("/categories")
+    public List<Category> getAllCategories() {
+        return categoryService.findAllCategories();
+    }
+
+    @GetMapping("/categories/{id}")
+    public Category getCategoryById(@PathVariable Long id) {
+        return categoryService.findCategoryById(id);
+    }
+
+    @PostMapping("/secure/categories")
+    public Category createCategory(@RequestBody Category category) {
+        return categoryService.saveCategory(category);
+    }
+
+    @PutMapping("/secure/categories/{id}")
+    public Category updateCategory(@PathVariable Long id, @RequestBody Category category) {
+        return categoryService.updateCategory(id, category);
+    }
+
+    @DeleteMapping("/secure/categories/{id}")
+    public void deleteCategory(@PathVariable Long id) {
+        categoryService.deleteCategory(id);
+    }
+}
