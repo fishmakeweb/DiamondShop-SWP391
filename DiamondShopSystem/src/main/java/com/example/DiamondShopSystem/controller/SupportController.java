3cd73891acf9991742b4e794cacfb5ccaa6f55c4
Nhat An <phungnhatan2204@gmail.com>
2024-06-05 22:07:06 +0700

    Modify Support controller

---
+++ DiamondShopSystem/src/main/java/com/example/DiamondShopSystem/controller/SupportController.java
@@ -0,0 +1,41 @@
+package com.example.DiamondShopSystem.controller;
+
+import com.example.DiamondShopSystem.model.Support;
+import com.example.DiamondShopSystem.service.SupportService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/supports")
+@CrossOrigin(origins = "http://localhost:3000")
+public class SupportController {
+    @Autowired
+    private SupportService supportService;
+
+    @GetMapping
+    public List<Support> getAllSupports() {
+        return supportService.findAllSupports();
+    }
+
+    @GetMapping("/{id}")
+    public Support getSupportById(@PathVariable Long id) {
+        return supportService.findSupportById(id);
+    }
+
+    @PostMapping
+    public Support createSupport(@RequestBody Support support) {
+        return supportService.saveSupport(support);
+    }
+
+    @PutMapping("/{id}")
+    public Support updateSupport(@PathVariable Long id, @RequestBody Support support) {
+        return supportService.updateSupport(id, support);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteSupport(@PathVariable Long id) {
+        supportService.deleteSupport(id);
+    }
+}
