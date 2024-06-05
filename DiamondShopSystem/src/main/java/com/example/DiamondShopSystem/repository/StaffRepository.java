d53847bed5c8144130b4771d7d0e34bf98c2353d
Nhat An <phungnhatan2204@gmail.com>
2024-06-05 21:41:10 +0700

    Modify Staff repository

--- DiamondShopSystem/src/main/java/com/example/DiamondShopSystem/repository/StaffRepository.java
+++ DiamondShopSystem/src/main/java/com/example/DiamondShopSystem/repository/StaffRepository.java
@@ -3,5 +3,9 @@ package com.example.DiamondShopSystem.repository;
 import com.example.DiamondShopSystem.model.Staff;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface StaffRepository extends JpaRepository<Staff, Long> {
+    Optional<Staff> findByUsername(String username);
+    void deleteByUsername(String username);
 }
