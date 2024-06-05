2b9bdd0a605408f41c095b0b93fa6b9ae9adf968
Nhat An <phungnhatan2204@gmail.com>
2024-06-05 22:38:41 +0700

    Add rout for logintoogle

--- DiamondShopSystem/customer-client/src/App.js
+++ DiamondShopSystem/customer-client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Homepage from './pages/Homepage';
 import Jewelry from './pages/Jewelry';
@@ -7,9 +7,17 @@ import ScrollToTop from './components/ScrollToTop';
 import Category from './pages/Category';
 import Gemstone from './pages/Gemstone';
 import CartContext from './components/CartContext';
-import { CartProvider } from './components/CartContext'; 
+import { CartProvider } from './components/CartContext';
+import LoginModal from './components/LoginModal';
+import UserProfile from './pages/UserProfile';
+import Staff from './pages/Staff'
 
 function App() {
+  const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
+
+  const toggleLoginModal = () => {
+    setIsLoginModalOpen(!isLoginModalOpen);
+  };
   return (
     <Router>
       <CartProvider> {/* Wrap Routes with CartProvider */}
@@ -21,7 +29,10 @@ function App() {
             <Route path="/jewelry/categories/:categoryid" element={<Category />} />
             <Route path="/jewelry/gemstones/:gemstoneid" element={<Gemstone />} />
             <Route path="/jewelry/:jewelryId" element={<JewelryItem />} />
+            <Route path="/profile" element={<UserProfile />} /> {/* Add profile route */}
+            <Route path="/staff" element={<Staff />}/>
           </Routes>
+          <LoginModal isOpen={isLoginModalOpen} onClose={toggleLoginModal} />
         </div>
       </CartProvider>
     </Router>
