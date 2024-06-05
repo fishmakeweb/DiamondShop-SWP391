41524897bd775190da7098842ffb17f3b9f2b1ab
Nhat An <phungnhatan2204@gmail.com>
2024-06-05 21:59:29 +0700

    Modify Carat Controller

---
+++ DiamondShopSystem/src/main/java/com/example/DiamondShopSystem/controller/CaratController.java
@@ -0,0 +1,41 @@
+package com.example.DiamondShopSystem.controller;
+
+import com.example.DiamondShopSystem.model.Carat;
+import com.example.DiamondShopSystem.service.CaratService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:3000")
+public class CaratController {
+    @Autowired
+    private CaratService caratService;
+
+    @GetMapping("/carats")
+    public List<Carat> getAllCarats() {
+        return caratService.findAllCarats();
+    }
+
+    @GetMapping("/carats/{id}")
+    public Carat getCaratById(@PathVariable Long id) {
+        return caratService.findCaratById(id);
+    }
+
+    @PostMapping("/secure/carats")
+    public Carat createCarat(@RequestBody Carat carat) {
+        return caratService.saveCarat(carat);
+    }
+
+    @PutMapping("/secure/carats/{id}")
+    public Carat updateCarat(@PathVariable Long id, @RequestBody Carat carat) {
+        return caratService.updateCarat(id, carat);
+    }
+
+    @DeleteMapping("/secure/carats/{id}")
+    public void deleteCarat(@PathVariable Long id) {
+        caratService.deleteCarat(id);
+    }
+}
