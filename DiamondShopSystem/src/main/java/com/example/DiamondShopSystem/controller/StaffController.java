e7c3b4e25dd36c936dfa49042861e3e21841ea90
Nhat An <phungnhatan2204@gmail.com>
2024-06-05 22:06:50 +0700

    Modify Staff controller

---
+++ DiamondShopSystem/src/main/java/com/example/DiamondShopSystem/controller/StaffController.java
@@ -0,0 +1,43 @@
+package com.example.DiamondShopSystem.controller;
+
+
+import com.example.DiamondShopSystem.model.Staff;
+import com.example.DiamondShopSystem.service.StaffService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/secure/staffs")
+@CrossOrigin(origins = "http://localhost:3000")
+public class StaffController {
+
+    @Autowired
+    private StaffService staffService;
+
+    @GetMapping
+    public List<Staff> getAllStaffs() {
+        return staffService.findAllStaffs();
+    }
+
+    @GetMapping("/{id}")
+    public Staff getStaffById(@PathVariable Long id) {
+        return staffService.findStaffById(id);
+    }
+
+    @PostMapping
+    public Staff createStaff(@RequestBody Staff Staff) {
+        return staffService.saveStaff(Staff);
+    }
+
+    @PutMapping("/{id}")
+    public Staff updateStaff(@PathVariable Long id, @RequestBody Staff Staff) {
+        return staffService.updateStaff(id, Staff);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteStaff(@PathVariable Long id) {
+        staffService.deleteStaff(id);
+    }
+}
