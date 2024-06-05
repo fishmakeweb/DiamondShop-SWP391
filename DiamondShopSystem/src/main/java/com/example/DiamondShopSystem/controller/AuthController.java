10365b863ec94fb37b82e75965d535823a5a5b5a
Nhat An <phungnhatan2204@gmail.com>
2024-06-05 21:51:01 +0700

    Add AuthController for endpoint of login,register

---
+++ DiamondShopSystem/src/main/java/com/example/DiamondShopSystem/controller/AuthController.java
@@ -0,0 +1,36 @@
+package com.example.DiamondShopSystem.controller;
+
+import com.example.DiamondShopSystem.dto.ReqRes;
+import com.example.DiamondShopSystem.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+@CrossOrigin(origins = "http://localhost:3000")
+public class AuthController {
+
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping("/register/staff")
+    public ResponseEntity<ReqRes> registerStaff(@RequestBody ReqRes req) {
+        return ResponseEntity.ok(authService.registerStaff(req));
+    }
+
+    @PostMapping("/register/customer")
+    public ResponseEntity<ReqRes> registerCustomer(@RequestBody ReqRes req) {
+        return ResponseEntity.ok(authService.registerCustomer(req));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<ReqRes> login(@RequestBody ReqRes req) {
+        return ResponseEntity.ok(authService.login(req));
+    }
+
+    @PostMapping("/refresh")
+    public ResponseEntity<ReqRes> refreshToken(@RequestBody ReqRes req) {
+        return ResponseEntity.ok(authService.refreshToken(req));
+    }
+}
