21c2c0b958150fd81fe86791758f7b15f3756c3e
Nhat An <phungnhatan2204@gmail.com>
2024-06-05 22:36:00 +0700

    Modify Delivery service

--- DiamondShopSystem/src/main/java/com/example/DiamondShopSystem/service/DeliveryService.java
+++ DiamondShopSystem/src/main/java/com/example/DiamondShopSystem/service/DeliveryService.java
@@ -1,14 +1,27 @@
 package com.example.DiamondShopSystem.service;
 
+import com.example.DiamondShopSystem.model.Cut;
 import com.example.DiamondShopSystem.model.Delivery;
 import com.example.DiamondShopSystem.repository.DeliveryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class DeliveryService {
     @Autowired
     private DeliveryRepository deliveryRepository;
+
+    public List<Delivery> getAllDeliveries(){
+        return deliveryRepository.findAll();
+    }
+
+    public Delivery getDeliveryById(Long id){
+        Optional<Delivery> delivery = deliveryRepository.findById(id);
+        return delivery.orElse(null);
+    }
 
     public Delivery saveDelivery(Delivery delivery) {
         return deliveryRepository.save(delivery);
