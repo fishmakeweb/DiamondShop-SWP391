646c6bc8f331e8fee18ddf446320b56dd177a194
Nhat An <phungnhatan2204@gmail.com>
2024-06-05 21:59:03 +0700

    Modify Article Controller

--- DiamondShopSystem/src/main/java/com/example/DiamondShopSystem/controller/ArticleController.java
+++ DiamondShopSystem/src/main/java/com/example/DiamondShopSystem/controller/ArticleController.java
@@ -4,36 +4,37 @@ import com.example.DiamondShopSystem.model.Article;
 import com.example.DiamondShopSystem.service.ArticleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/articles")
+@RequestMapping("/api")
 @CrossOrigin(origins = "http://localhost:3000")
 public class ArticleController {
     @Autowired
     private ArticleService articleService;
 
-    @GetMapping
+    @GetMapping("/articles")
     public List<Article> getAllArticles() {
         return articleService.findAllArticles();
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/articles/{id}")
     public Article getArticleById(@PathVariable Long id) {
         return articleService.findArticleById(id);
     }
 
-    @PostMapping
+    @PostMapping("/secure/articles")
     public Article createArticle(@RequestBody Article article) {
         return articleService.saveArticle(article);
     }
 
-    @PutMapping("/{id}")
+    @PutMapping("/secure/articles/{id}")
     public Article updateArticle(@PathVariable Long id, @RequestBody Article article) {
         return articleService.updateArticle(id, article);
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/secure/articles/{id}")
     public void deleteArticle(@PathVariable Long id) {
         articleService.deleteArticle(id);
     }
