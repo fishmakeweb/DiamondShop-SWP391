524984d9db99afd280e7350114012ae7f73c6ea5
tdatj <127045484+tdatj@users.noreply.github.com>
2024-06-13 22:02:29 +0700

    Update cut

--- DiamondShopSystem/customer-client/src/pages/diamond.js
+++ DiamondShopSystem/customer-client/src/pages/diamond.js
@@ -9,8 +9,8 @@ import RangeSlider from "../components/RangeSlider.js";
 function Diamond() {
   const colorGrades = ['D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'];
   const clarityGrades = ['FL', 'IF', 'VVS1', 'VVS2', 'VS1', 'VS2', 'SI1', 'SI2', 'I1', 'I2'];
-  const polishGrades = ['Fair','Good', 'Poor','Very Good'];
-  const fluorescenceGrades = ['Faint', 'Medium', 'Strong', 'Very Strong'];
+  const cutGrades = ['Fair', 'Good', 'Very Good', 'Excellent'];
+
   const [selectedShapes, setSelectedShapes] = useState([]);
   const [priceFrom, setPriceFrom] = useState("");
   const [priceTo, setPriceTo] = useState("");
@@ -24,13 +24,8 @@ function Diamond() {
   const [maxColor, setMaxColor] = useState(9); // Default to M
   const [minClarity, setMinClarity] = useState(0); // Default to FL
   const [maxClarity, setMaxClarity] = useState(9); // Default to I2
-  const [minPolish, setMinPolish] = useState(0); // Default to Fair
-  const [maxPolish, setMaxPolish] = useState(3); // Default to Very Good
-  const [minFluorescence, setMinFluorescence] = useState(0); // Default to Faint
-  const [maxFluorescence, setMaxFluorescence] = useState(3); // Default to Very Strong
-
-  
-
+  const [minCut, setMinCut] = useState(0); // Default to Fair
+  const [maxCut, setMaxCut] = useState(3); // Default to Excellent
 
   useEffect(() => {
     axios.get('/diamonds')
@@ -43,47 +38,48 @@ function Diamond() {
 
   useEffect(() => {
     let filtered = diamonds;
-  
+
     if (selectedShapes.length > 0) {
       filtered = filtered.filter(diamond => selectedShapes.includes(diamond.shape.shapeDescription));
     }
-  
+
     if (caratFrom !== "") {
       filtered = filtered.filter(diamond => diamond.carat.carat >= parseFloat(caratFrom));
     }
-  
+
     if (caratTo !== "") {
       filtered = filtered.filter(diamond => diamond.carat.carat <= parseFloat(caratTo));
     }
-  
+
     if (priceFrom !== "") {
       filtered = filtered.filter(diamond => diamond.price >= parseFloat(priceFrom));
     }
-  
+
     if (priceTo !== "") {
       filtered = filtered.filter(diamond => diamond.price <= parseFloat(priceTo));
     }
-  
+
     if (millimetreFrom !== "") {
       filtered = filtered.filter(diamond => diamond.measurement.length >= parseFloat(millimetreFrom));
     }
-  
+
     if (millimetreTo !== "") {
       filtered = filtered.filter(diamond => diamond.measurement.length <= parseFloat(millimetreTo));
     }
-  
+
     const selectedColorGrades = colorGrades.slice(minColor, maxColor + 1);
     const selectedClarityGrades = clarityGrades.slice(minClarity, maxClarity + 1);
-    const selectedPolishGrades = polishGrades.slice(minPolish, maxPolish + 1);
-    const selectedFluorescenceGrades = fluorescenceGrades.slice(minFluorescence, maxFluorescence + 1);
-  
+    const selectedCutGrades = cutGrades.slice(minCut, maxCut + 1);
+
     filtered = filtered.filter(diamond => selectedColorGrades.includes(diamond.color.colorDescription));
     filtered = filtered.filter(diamond => selectedClarityGrades.includes(diamond.clarity.clarityDescription));
-    filtered = filtered.filter(diamond => selectedPolishGrades.includes(diamond.polish.polishDescription));
-    filtered = filtered.filter(diamond => selectedFluorescenceGrades.includes(diamond.fluorescence.fluorescenceDescription));
-  
+    filtered = filtered.filter(diamond => selectedCutGrades.includes(diamond.cut.cutDescription));
+
     setFilteredDiamonds(filtered);
-  }, [selectedShapes, priceFrom, priceTo, caratFrom, caratTo, millimetreFrom, millimetreTo, minColor, maxColor, minClarity, maxClarity, minPolish, maxPolish, minFluorescence, maxFluorescence, diamonds]);
+  }, [
+    selectedShapes, priceFrom, priceTo, caratFrom, caratTo, millimetreFrom, millimetreTo,
+    minColor, maxColor, minClarity, maxClarity, minCut, maxCut, diamonds
+  ]);
 
   function handleShapeClick(shape) {
     setSelectedShapes(prevSelectedShapes => {
@@ -94,8 +90,6 @@ function Diamond() {
       }
     });
   }
-
-  console.log(filteredDiamonds)
 
   return (
     <div className="flex overflow-hidden flex-col items-center pb-20 bg-white z-0">
@@ -169,55 +163,69 @@ function Diamond() {
           </div>
         </div>
 
-        <div className="flex gap-5 justify-between mt-24 w-full text-base whitespace-nowrap text-stone-300 max-md:flex-wrap max-md:mt-10 max-md:max-w-full">
-  <div className="flex gap-5 max-md:flex-wrap max-md:max-w-full w-full">
-    <div className="w-full max-w-[550px]">
-      <RangeSlider 
-        label="Color"
-        grades={colorGrades}
-        minGrade={minColor}
-        maxGrade={maxColor}
-        setMinGrade={setMinColor}
-        setMaxGrade={setMaxColor}
-      />
-    </div>
-    <div className="w-full max-w-[550px] ml-60">
-      <RangeSlider 
-        label="Clarity"
-        grades={clarityGrades}
-        minGrade={minClarity}
-        maxGrade={maxClarity}
-        setMinGrade={setMinClarity}
-        setMaxGrade={setMaxClarity}
-      />
-    </div>
-  </div>
-</div>
+       
 
-<div className="flex gap-5 justify-between mt-24 w-full text-base whitespace-nowrap text-stone-300 max-md:flex-wrap max-md:mt-10 max-md:max-w-full">
-  <div className="flex gap-5 max-md:flex-wrap max-md:max-w-full w-full">
-    <div className="w-full max-w-[550px]">
-      <RangeSlider 
-        label="Polish"
-        grades={polishGrades}
-        minGrade={minPolish}
-        maxGrade={maxPolish}
-        setMinGrade={setMinPolish}
-        setMaxGrade={setMaxPolish}
-      />
-    </div>
-    <div className="w-full max-w-[550px] ml-60">
-      <RangeSlider 
-        label="Fluorescence"
-        grades={fluorescenceGrades}
-        minGrade={minFluorescence}
-        maxGrade={maxFluorescence}
-        setMinGrade={setMinFluorescence}
-        setMaxGrade={setMaxFluorescence}
-      />
-    </div>
-  </div>
-</div>
+        <div className="flex gap-5 justify-between mt-24 w-full text-base whitespace-nowrap text-stone-300 max-md:flex-wrap max-md:mt-10 max-md:max-w-full">
+          <div className="flex gap-5 max-md:flex-wrap max-md:max-w-full w-full">
+            <div className="w-full max-w-[550px]">
+              <RangeSlider 
+                label="Color"
+                grades={colorGrades}
+                minGrade={minColor}
+                maxGrade={maxColor}
+                setMinGrade={setMinColor}
+                setMaxGrade={setMaxColor}
+              />
+            </div>
+            <div className="w-full max-w-[550px] ml-60">
+              <RangeSlider 
+                label="Clarity"
+                grades={clarityGrades}
+                minGrade={minClarity}
+                maxGrade={maxClarity}
+                setMinGrade={setMinClarity}
+                setMaxGrade={setMaxClarity}
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="flex gap-5 justify-between mt-24 w-full text-base whitespace-nowrap text-stone-300 max-md:flex-wrap max-md:mt-10 max-md:max-w-full">
+          <div className="flex gap-5 max-md:flex-wrap max-md:max-w-full w-full">
+            <div className="w-full max-w-[550px]">
+              <RangeSlider 
+                label="Cut"
+                grades={cutGrades}
+                minGrade={minCut}
+                maxGrade={maxCut}
+                setMinGrade={setMinCut}
+                setMaxGrade={setMaxCut}
+              />
+            </div>
+          </div>
+           <div className="flex gap-5 justify-end items-center w-full text-base whitespace-nowrap text-stone-300 max-md:flex-wrap max-md:mt-10 max-md:max-w-full">
+          <div className="flex gap-5 max-md:flex-wrap max-md:max-w-full">
+            <div className="grow text-3xl text-black">
+              Millimeter
+            </div>
+            <input
+              type="number"
+              value={millimetreFrom}
+              onChange={(e) => setMillimetreFrom(e.target.value)}
+              className="number-input justify-center items-start bg-white border border-solid backdrop-blur-[2px] border-stone-300 max-md:pr-5"
+              placeholder="From"
+            />
+            <input
+              type="number"
+              value={millimetreTo}
+              onChange={(e) => setMillimetreTo(e.target.value)}
+              className="number-input justify-center items-center bg-white border border-solid border-stone-300 max-md:pr-5"
+              placeholder="To"
+            />
+          </div>
+        </div>
+        </div>
+        
 
         <DiamondList items={filteredDiamonds} />
       </div>
