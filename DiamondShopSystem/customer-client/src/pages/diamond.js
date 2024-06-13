4cb20270defd6f0487c46761394cb58971bf4948
Thành Đạt Võ <ahihi324@gmail.com>
2024-06-13 13:01:53 +0700

    Update diamond Page

--- DiamondShopSystem/customer-client/src/pages/diamond.js
+++ DiamondShopSystem/customer-client/src/pages/diamond.js
@@ -3,9 +3,14 @@ import Navbar from "../components/Navbar.js";
 import Footer from "../components/Footer.js";
 import DiamondList from "../components/DiamondList";
 import axios from '../axios.js';
-import '../style/Diamond.css'; // Ensure the CSS file is imported
+import '../style/Diamond.css'; 
+import RangeSlider from "../components/RangeSlider.js";
 
 function Diamond() {
+  const colorGrades = ['D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'];
+  const clarityGrades = ['FL', 'IF', 'VVS1', 'VVS2', 'VS1', 'VS2', 'SI1', 'SI2', 'I1', 'I2'];
+  const polishGrades = ['Fair','Good', 'Poor','Very Good'];
+  const fluorescenceGrades = ['Faint', 'Medium', 'Strong', 'Very Strong'];
   const [selectedShapes, setSelectedShapes] = useState([]);
   const [priceFrom, setPriceFrom] = useState("");
   const [priceTo, setPriceTo] = useState("");
@@ -13,6 +18,19 @@ function Diamond() {
   const [caratTo, setCaratTo] = useState("");
   const [diamonds, setDiamonds] = useState([]);
   const [filteredDiamonds, setFilteredDiamonds] = useState([]);
+  const [millimetreFrom, setMillimetreFrom] = useState("");
+  const [millimetreTo, setMillimetreTo] = useState("");
+  const [minColor, setMinColor] = useState(0); // Default to D
+  const [maxColor, setMaxColor] = useState(9); // Default to M
+  const [minClarity, setMinClarity] = useState(0); // Default to FL
+  const [maxClarity, setMaxClarity] = useState(9); // Default to I2
+  const [minPolish, setMinPolish] = useState(0); // Default to Fair
+  const [maxPolish, setMaxPolish] = useState(3); // Default to Very Good
+  const [minFluorescence, setMinFluorescence] = useState(0); // Default to Faint
+  const [maxFluorescence, setMaxFluorescence] = useState(3); // Default to Very Strong
+
+  
+
 
   useEffect(() => {
     axios.get('/diamonds')
@@ -23,31 +41,49 @@ function Diamond() {
       .catch(error => console.error('Error fetching diamond data:', error));
   }, []);
 
-  function handleFilter() {
+  useEffect(() => {
     let filtered = diamonds;
-
+  
     if (selectedShapes.length > 0) {
       filtered = filtered.filter(diamond => selectedShapes.includes(diamond.shape.shapeDescription));
     }
-
+  
     if (caratFrom !== "") {
       filtered = filtered.filter(diamond => diamond.carat.carat >= parseFloat(caratFrom));
     }
-
+  
     if (caratTo !== "") {
       filtered = filtered.filter(diamond => diamond.carat.carat <= parseFloat(caratTo));
     }
-
+  
     if (priceFrom !== "") {
       filtered = filtered.filter(diamond => diamond.price >= parseFloat(priceFrom));
     }
-
+  
     if (priceTo !== "") {
       filtered = filtered.filter(diamond => diamond.price <= parseFloat(priceTo));
     }
-
+  
+    if (millimetreFrom !== "") {
+      filtered = filtered.filter(diamond => diamond.measurement.length >= parseFloat(millimetreFrom));
+    }
+  
+    if (millimetreTo !== "") {
+      filtered = filtered.filter(diamond => diamond.measurement.length <= parseFloat(millimetreTo));
+    }
+  
+    const selectedColorGrades = colorGrades.slice(minColor, maxColor + 1);
+    const selectedClarityGrades = clarityGrades.slice(minClarity, maxClarity + 1);
+    const selectedPolishGrades = polishGrades.slice(minPolish, maxPolish + 1);
+    const selectedFluorescenceGrades = fluorescenceGrades.slice(minFluorescence, maxFluorescence + 1);
+  
+    filtered = filtered.filter(diamond => selectedColorGrades.includes(diamond.color.colorDescription));
+    filtered = filtered.filter(diamond => selectedClarityGrades.includes(diamond.clarity.clarityDescription));
+    filtered = filtered.filter(diamond => selectedPolishGrades.includes(diamond.polish.polishDescription));
+    filtered = filtered.filter(diamond => selectedFluorescenceGrades.includes(diamond.fluorescence.fluorescenceDescription));
+  
     setFilteredDiamonds(filtered);
-  }
+  }, [selectedShapes, priceFrom, priceTo, caratFrom, caratTo, millimetreFrom, millimetreTo, minColor, maxColor, minClarity, maxClarity, minPolish, maxPolish, minFluorescence, maxFluorescence, diamonds]);
 
   function handleShapeClick(shape) {
     setSelectedShapes(prevSelectedShapes => {
@@ -59,16 +95,18 @@ function Diamond() {
     });
   }
 
+  console.log(filteredDiamonds)
+
   return (
     <div className="flex overflow-hidden flex-col items-center pb-20 bg-white z-0">
       <div className="self-stretch w-full bg-black min-h-[32px] max-md:max-w-full" />
-        <div className="mt-36 text-4xl text-center text-black max-md:mt-10">
-          Diamond
-        </div>
-        <div className="mt-8 text-xl text-center text-stone-500">
-          Lorem ipsum dolor sit amet.
-        </div>
-        
+      <div className="mt-36 text-4xl text-center text-black max-md:mt-10">
+        Diamond
+      </div>
+      <div className="mt-8 text-xl text-center text-stone-500">
+        Lorem ipsum dolor sit amet.
+      </div>
+      
       <div className=" flex flex-col mt-32 w-full max-w-[1265px] max-md:mt-10 max-md:max-w-full">
         <div className="flex gap-5 w-full max-md:flex-wrap max-md:max-w-full">
           <div className="my-auto text-3xl text-black z-0">Shape</div>
@@ -91,6 +129,7 @@ function Diamond() {
             ))}
           </div>
         </div>
+
         <div className="flex gap-5 justify-between mt-24 w-full text-base whitespace-nowrap text-stone-300 max-md:flex-wrap max-md:mt-10 max-md:max-w-full">
           <div className="flex gap-5 max-md:flex-wrap max-md:max-w-full">
             <div className="grow text-3xl text-black">Price</div>
@@ -109,8 +148,8 @@ function Diamond() {
               placeholder="To"
             />
           </div>
-          <div className="flex gap-5 justify-between max-md:flex-wrap max-md:max-w-full">
-            <div className="self-start mt-3 text-3xl text-center text-black">
+          <div className="flex gap-5 max-md:flex-wrap max-md:max-w-full">
+            <div className="grow text-3xl text-black">
               Carat
             </div>
             <input
@@ -124,17 +163,62 @@ function Diamond() {
               type="number"
               value={caratTo}
               onChange={(e) => setCaratTo(e.target.value)}
-              className="number-input justify-center items-start bg-white border border-solid backdrop-blur-[2px] border-stone-300 max-md:pr-5"
+              className="number-input justify-center items-start bg-white border border-solid border-stone-300 max-md:pr-5"
               placeholder="To"
             />
           </div>
         </div>
-        <button
-          className="justify-center self-center px-7 py-4 mt-7 ml-14 text-2xl text-black bg-orange-200 rounded-xl max-md:pr-5"
-          onClick={handleFilter}
-        >
-          Search
-        </button>
+
+        <div className="flex gap-5 justify-between mt-24 w-full text-base whitespace-nowrap text-stone-300 max-md:flex-wrap max-md:mt-10 max-md:max-w-full">
+  <div className="flex gap-5 max-md:flex-wrap max-md:max-w-full w-full">
+    <div className="w-full max-w-[550px]">
+      <RangeSlider 
+        label="Color"
+        grades={colorGrades}
+        minGrade={minColor}
+        maxGrade={maxColor}
+        setMinGrade={setMinColor}
+        setMaxGrade={setMaxColor}
+      />
+    </div>
+    <div className="w-full max-w-[550px] ml-60">
+      <RangeSlider 
+        label="Clarity"
+        grades={clarityGrades}
+        minGrade={minClarity}
+        maxGrade={maxClarity}
+        setMinGrade={setMinClarity}
+        setMaxGrade={setMaxClarity}
+      />
+    </div>
+  </div>
+</div>
+
+<div className="flex gap-5 justify-between mt-24 w-full text-base whitespace-nowrap text-stone-300 max-md:flex-wrap max-md:mt-10 max-md:max-w-full">
+  <div className="flex gap-5 max-md:flex-wrap max-md:max-w-full w-full">
+    <div className="w-full max-w-[550px]">
+      <RangeSlider 
+        label="Polish"
+        grades={polishGrades}
+        minGrade={minPolish}
+        maxGrade={maxPolish}
+        setMinGrade={setMinPolish}
+        setMaxGrade={setMaxPolish}
+      />
+    </div>
+    <div className="w-full max-w-[550px] ml-60">
+      <RangeSlider 
+        label="Fluorescence"
+        grades={fluorescenceGrades}
+        minGrade={minFluorescence}
+        maxGrade={maxFluorescence}
+        setMinGrade={setMinFluorescence}
+        setMaxGrade={setMaxFluorescence}
+      />
+    </div>
+  </div>
+</div>
+
         <DiamondList items={filteredDiamonds} />
       </div>
       <Navbar />
