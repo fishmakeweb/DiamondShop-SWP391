91b07354b0f8b0cfc76bddfec712f7801f07cee5
Nhat An <phungnhatan2204@gmail.com>
2024-06-15 11:03:49 +0700

    Create form add jewelry, diamond

---
+++ DiamondShopSystem/src/main/java/com/example/DiamondShopSystem/dto/AllDataDTO.java
@@ -0,0 +1,43 @@
+package com.example.DiamondShopSystem.dto;
+
+import com.example.DiamondShopSystem.model.Category;
+import com.example.DiamondShopSystem.model.Material;
+import com.example.DiamondShopSystem.model.Size;
+
+import java.util.List;
+
+public class AllDataDTO {
+    private List<Category> categories;
+    private List<Material> materials;
+    private List<Size> sizes;
+
+    public AllDataDTO(List<Category> categories, List<Material> materials, List<Size> sizes) {
+        this.categories = categories;
+        this.materials = materials;
+        this.sizes = sizes;
+    }
+
+    public List<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(List<Category> categories) {
+        this.categories = categories;
+    }
+
+    public List<Material> getMaterials() {
+        return materials;
+    }
+
+    public void setMaterials(List<Material> materials) {
+        this.materials = materials;
+    }
+
+    public List<Size> getSizes() {
+        return sizes;
+    }
+
+    public void setSizes(List<Size> sizes) {
+        this.sizes = sizes;
+    }
+}
