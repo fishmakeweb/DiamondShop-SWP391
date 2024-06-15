5de3c45677d938ad9480d7c9ca25424438110ec4
Nhat An <phungnhatan2204@gmail.com>
2024-06-15 11:02:21 +0700

    Delete some attributes of diamond, jewelry

--- DiamondShopSystem/src/main/java/com/example/DiamondShopSystem/repository/FluorescenceRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.DiamondShopSystem.repository;
-
-import com.example.DiamondShopSystem.model.Fluorescence;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FluorescenceRepository extends JpaRepository<Fluorescence, Long> {
-}
