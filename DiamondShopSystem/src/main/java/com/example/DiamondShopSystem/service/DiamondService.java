66198bc71a6c127ab572caa77852f91190afdac5
Nhat An <phungnhatan2204@gmail.com>
2024-06-15 11:12:41 +0700

    Update Diamond api

--- DiamondShopSystem/src/main/java/com/example/DiamondShopSystem/service/DiamondService.java
+++ DiamondShopSystem/src/main/java/com/example/DiamondShopSystem/service/DiamondService.java
@@ -1,9 +1,14 @@
 package com.example.DiamondShopSystem.service;
 
+import com.example.DiamondShopSystem.dto.DiamondAttributeDTO;
 import com.example.DiamondShopSystem.model.Diamond;
-import com.example.DiamondShopSystem.repository.DiamondRepository;
+import com.example.DiamondShopSystem.model.Gia;
+import com.example.DiamondShopSystem.model.Jewelry;
+import com.example.DiamondShopSystem.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.util.List;
 import java.util.Optional;
 
@@ -12,6 +17,29 @@ public class DiamondService {
     @Autowired
     private DiamondRepository diamondRepository;
 
+    @Autowired
+    private GiaRepository giaRepository;
+
+    @Autowired
+    private ProductService productService;
+
+    @Autowired
+    private ShapeRepository shapeRepository;
+
+    @Autowired
+    private MeasurementRepository measurementRepository;
+
+    @Autowired
+    private ColorRepository colorRepository;
+
+    @Autowired
+    private CutRepository cutRepository;
+
+    @Autowired
+    private CaratRepository caratRepository;
+
+    @Autowired
+    private ClarityRepository clarityRepository;
     public List<Diamond> findAllDiamonds() {
         return diamondRepository.findAll();
     }
@@ -21,8 +49,12 @@ public class DiamondService {
         return diamond.orElse(null);
     }
 
+
+    @Transactional
     public Diamond saveDiamond(Diamond diamond) {
-        return diamondRepository.save(diamond);
+        Diamond savedDiamond = diamondRepository.save(diamond);
+        productService.createProductForDiamond(savedDiamond);
+        return savedDiamond;
     }
 
     public Diamond updateDiamond(Long id, Diamond newDiamond) {
@@ -34,9 +66,6 @@ public class DiamondService {
                     existingDiamond.setColor(newDiamond.getColor());
                     existingDiamond.setCut(newDiamond.getCut());
                     existingDiamond.setClarity(newDiamond.getClarity());
-                    existingDiamond.setPolish(newDiamond.getPolish());
-                    existingDiamond.setSymmetry(newDiamond.getSymmetry());
-                    existingDiamond.setFluorescence(newDiamond.getFluorescence());
                     existingDiamond.setPrice(newDiamond.getPrice());
                     existingDiamond.setImg(newDiamond.getImg());
                     return diamondRepository.save(existingDiamond);
@@ -48,5 +77,17 @@ public class DiamondService {
 
     public void deleteDiamond(Long id) {
         diamondRepository.deleteById(id);
+    }
+
+
+    public DiamondAttributeDTO getAllDiamondAttributes() {
+        DiamondAttributeDTO dto = new DiamondAttributeDTO();
+        dto.setShapes(shapeRepository.findAll());
+        dto.setMeasurements(measurementRepository.findAll());
+        dto.setColors(colorRepository.findAll());
+        dto.setCuts(cutRepository.findAll());
+        dto.setCarats(caratRepository.findAll());
+        dto.setClarities(clarityRepository.findAll());
+        return dto;
     }
 }
