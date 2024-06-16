cd2a06df03408b914ee1956fd5f890b7f5c564e0
PhatNLT <letienphat03@gmail.com>
2024-06-16 17:24:46 +0700

    OrderStatus repo service and controller

---
+++ DiamondShopSystem/src/main/java/com/example/DiamondShopSystem/service/OrderStatusService.java
@@ -0,0 +1,44 @@
+package com.example.DiamondShopSystem.service;
+
+import com.example.DiamondShopSystem.model.OrderStatus;
+import com.example.DiamondShopSystem.repository.OrderStatusRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class OrderStatusService {
+
+    @Autowired
+    private OrderStatusRepository orderStatusRepository;
+
+    public List<OrderStatus> findAllOrderStatuses() {
+        return orderStatusRepository.findAll();
+    }
+
+    public OrderStatus findOrderStatusById(Long id) {
+        Optional<OrderStatus> orderStatus = orderStatusRepository.findById(id);
+        return orderStatus.orElse(null);
+    }
+
+    public OrderStatus saveOrderStatus(OrderStatus orderStatus) {
+        return orderStatusRepository.save(orderStatus);
+    }
+
+    public OrderStatus updateOrderStatus(Long id, OrderStatus newOrderStatus) {
+        return orderStatusRepository.findById(id)
+                .map(existingOrderStatus -> {
+                    existingOrderStatus.setStatusDescription(newOrderStatus.getStatusDescription());
+                    return orderStatusRepository.save(existingOrderStatus);
+                }).orElseGet(() -> {
+                    newOrderStatus.setStatusId(id);  // Ensure to set the correct ID
+                    return orderStatusRepository.save(newOrderStatus);
+                });
+    }
+
+    public void deleteOrderStatus(Long id) {
+        orderStatusRepository.deleteById(id);
+    }
+}
