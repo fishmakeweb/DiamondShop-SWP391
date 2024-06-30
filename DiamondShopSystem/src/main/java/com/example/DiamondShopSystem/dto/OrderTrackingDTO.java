697f8de38f75c1ad50bf3568b4588f35a32cf9dc
PhatNLT <letienphat03@gmail.com>
2024-06-30 20:35:03 +0700

    Create OrderTracking DTO

---
+++ DiamondShopSystem/src/main/java/com/example/DiamondShopSystem/dto/OrderTrackingDTO.java
@@ -0,0 +1,33 @@
+package com.example.DiamondShopSystem.dto;
+
+import java.util.Date;
+
+public class OrderTrackingDTO {
+    private Long orderId;
+    private Date orderDate;
+    private float totalPrice;
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public Date getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(Date orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public float getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(float totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+}
