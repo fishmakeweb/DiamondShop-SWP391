3c16007adcb644cc97c8bad6b505b5c17c01f39e
= <phungnhatan2204@gmail.com>
2024-07-02 11:36:57 +0700

    Config OAuth2

--- DiamondShopSystem/src/main/java/com/example/DiamondShopSystem/config/SecurityConfig.java
+++ DiamondShopSystem/src/main/java/com/example/DiamondShopSystem/config/SecurityConfig.java
@@ -1,6 +1,10 @@
 package com.example.DiamondShopSystem.config;
 
+import com.example.DiamondShopSystem.repository.OrderRepository;
+import com.example.DiamondShopSystem.service.CustomOAuth2UserService;
 import com.example.DiamondShopSystem.service.CustomUserDetailsService;
+import com.example.DiamondShopSystem.service.CustomerService;
+import com.example.DiamondShopSystem.service.JWTUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,37 +17,62 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     @Autowired
+    private CustomOAuth2UserService customOAuth2UserService;
+
+    @Autowired
     private CustomUserDetailsService customUserDetailsService;
+
 
     @Autowired
     private JWTAuthFilter jwtAuthFilter;
 
+    @Autowired
+    private JWTUtils jwtUtils;
+
+   @Autowired
+   OrderRepository orderRepository;
+
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity, OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler) throws Exception {
         httpSecurity.csrf(AbstractHttpConfigurer::disable)
                 .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(request -> request
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/api/**").permitAll()
                         .requestMatchers("/api/secure/**").hasAnyAuthority("ADMIN", "MANAGER", "SALESTAFF", "DELIVERYSTAFF")
-                        .anyRequest().authenticated())
+                        .anyRequest().authenticated()
+                        )
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationProvider())
-                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .oauth2Login(oauth2 -> oauth2
+                        .loginPage("http://localhost:3000/login")
+                        .userInfoEndpoint(userInfo -> userInfo.userService(customOAuth2UserService))
+                        .successHandler(oAuth2LoginSuccessHandler)
+                );
+
+
 
         return httpSecurity.build();
     }
-
+    @Bean
+    public OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler(CustomerService customerService) {
+        return new OAuth2LoginSuccessHandler(customerService, jwtUtils, orderRepository);
+    }
     @Bean
     public AuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
@@ -61,4 +90,6 @@ public class SecurityConfig {
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
     }
+
+
 }
