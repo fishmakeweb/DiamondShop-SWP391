2eca150550e91a9db7089db69e5ea464b7f9d29a
= <phungnhatan2204@gmail.com>
2024-07-04 18:34:02 +0700

    Modify ReqRes

--- DiamondShopSystem/src/main/java/com/example/DiamondShopSystem/dto/ReqRes.java
+++ DiamondShopSystem/src/main/java/com/example/DiamondShopSystem/dto/ReqRes.java
@@ -3,7 +3,7 @@ package com.example.DiamondShopSystem.dto;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
-import java.sql.Date;
+import java.util.Date;
 import java.util.List;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
