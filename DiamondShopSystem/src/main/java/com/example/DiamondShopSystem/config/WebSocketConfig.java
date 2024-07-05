5b4fe9e8f283cff32c15725391102a723c763ab7
Viet Hoang <duongviethoang.1909@gmail.com>
2024-07-05 21:34:26 +0700

    Update cors config test server

--- DiamondShopSystem/src/main/java/com/example/DiamondShopSystem/config/WebSocketConfig.java
+++ DiamondShopSystem/src/main/java/com/example/DiamondShopSystem/config/WebSocketConfig.java
@@ -14,7 +14,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         // Make sure to allow CORS here, adjust origins as needed
         registry.addEndpoint("/chat")
-                .setAllowedOrigins("http://localhost:3000")
+                .setAllowedOrigins("https://hepheathus.store")
                 .withSockJS();
     }
 
