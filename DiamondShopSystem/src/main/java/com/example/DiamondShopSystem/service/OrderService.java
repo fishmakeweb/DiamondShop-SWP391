382749a5fc3ab0feebc38b1eaa1c1f6c4f233352
Viet Hoang <duongviethoang.1909@gmail.com>
2024-07-06 16:21:35 +0700

    Update create checkout link, confirm check out function, create new cart after checkout

--- DiamondShopSystem/src/main/java/com/example/DiamondShopSystem/service/OrderService.java
+++ DiamondShopSystem/src/main/java/com/example/DiamondShopSystem/service/OrderService.java
@@ -3,12 +3,15 @@ package com.example.DiamondShopSystem.service;
 import com.example.DiamondShopSystem.dto.OrderDTO;
 import com.example.DiamondShopSystem.model.Order;
 import com.example.DiamondShopSystem.model.OrderChatMessage;
-import com.example.DiamondShopSystem.repository.CustomerRepository;
-import com.example.DiamondShopSystem.repository.OrderChatMessageRepository;
-import com.example.DiamondShopSystem.repository.OrderDetailRepository;
-import com.example.DiamondShopSystem.repository.OrderRepository;
+import com.example.DiamondShopSystem.model.OrderStatus;
+import com.example.DiamondShopSystem.model.PaymentRequest;
+import com.example.DiamondShopSystem.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.Instant;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -29,6 +32,12 @@ public class OrderService {
     @Autowired
     private OrderChatMessageRepository orderChatMessageRepository;
 
+    @Autowired
+    private OrderStatusRepository orderStatusRepository;
+
+    @Autowired
+    private PaymentRequestRepository paymentRequestRepository;
+
     public List<Order> findAllOrders() {
         return orderRepository.findAll();
     }
@@ -46,9 +55,6 @@ public class OrderService {
         orderRepository.deleteById(id);
     }
 
-
-
-
     public Order saveOrderOnCheckOut(Order order){
         //Not completed
         return new Order();
@@ -62,6 +68,20 @@ public class OrderService {
         // Return an empty list if there is no active order
     }
 
+    public void setSuccessStatus(Order order) {
+        OrderStatus successStatus = orderStatusRepository.findById(4L).get();
+        order.setOrderStatus(successStatus);
+        order.setOrderDate(new Date());
+        orderRepository.save(order);
+    }
+
+    public void createNewCart(Order finishOrder) {
+        Order order = new Order();
+        order.setUsername(finishOrder.getUsername());
+        order.setOrderStatus(orderStatusRepository.findById(1L).get());
+        orderRepository.save(order);
+    }
+
     public List<Order> getOrders(String token) {
         String username = jwtUtils.extractUsername(token);
         return orderRepository.findFinshiedOrderByUsername(username);
@@ -72,5 +92,41 @@ public class OrderService {
     public void saveChatMessage(OrderChatMessage message) {
         // Here, you might add any business logic before saving the message
         orderChatMessageRepository.save(message);
+    }
+
+    public String checkOut(String token) {
+        String username = jwtUtils.extractUsername(token);
+        Optional<Order> activeOrder = orderRepository.findActiveOrderByUsername(username);
+        Order order = activeOrder.get();
+        double totalPrice = order.getTotalPrice();
+        PaymentRequest paymentRequest = new PaymentRequest();
+        paymentRequest.setAmount(totalPrice);
+        paymentRequest.setDescription("Hephaestus Order " + order.getOrderId());
+        paymentRequest.setExpiredAt(Instant.now().plusSeconds(300).getEpochSecond());
+        paymentRequest.setReturnUrl("https://hephaestus.store/Success?payToken="+jwtUtils.generateToken(order));
+        paymentRequest.setCancelUrl("https://hephaestus.store/Cancelled");
+        paymentRequest = paymentRequestRepository.save(paymentRequest);
+        System.out.println(paymentRequest.toString());
+        String createPaymentUrl = "https://payment.hephaestus.store/create-payment-link";
+        RestTemplate restTemplate = new RestTemplate();
+        String paymentUrl = restTemplate.postForObject(createPaymentUrl,paymentRequest,String.class);
+        return paymentUrl;
+    }
+
+    public String successCheckOut(String token) {
+        String username = jwtUtils.extractUsername(token);
+        Optional<Order> activeOrder = orderRepository.findActiveOrderByUsername(username);
+        if ((activeOrder.isPresent())) {
+            Order order = activeOrder.get();
+            if (!jwtUtils.isTokenExpired(token)) {
+                setSuccessStatus(order);
+                createNewCart(order);
+                return "Check Out successfully";
+            } else {
+                return "This token is expired";
+            }
+        } else {
+            return "This order is not exists!!";
+        }
     }
 }
