bf242e850cc5c80ed3ec0fc1835187204c9b8eee
Viet Hoang <duongviethoang.1909@gmail.com>
2024-07-06 16:15:42 +0700

    Update get jewelry page api

--- DiamondShopSystem/src/main/java/com/example/DiamondShopSystem/controller/ProductController.java
+++ DiamondShopSystem/src/main/java/com/example/DiamondShopSystem/controller/ProductController.java
@@ -4,9 +4,11 @@ import com.example.DiamondShopSystem.dto.JewelryDTO;
 import com.example.DiamondShopSystem.dto.NewReleaseDTO;
 import com.example.DiamondShopSystem.model.Product;
 import com.example.DiamondShopSystem.model.ProductDetails;
+import com.example.DiamondShopSystem.service.JewelryService;
 import com.example.DiamondShopSystem.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,20 +21,24 @@ import java.util.List;
 public class ProductController {
     @Autowired
     private ProductService productService;
+    @Autowired
+    private JewelryService jewelryService;
 
     @GetMapping("/products")
     public List<Product> getAllProducts() {
         return productService.findAllProducts();
     }
 
-
-//    @GetMapping("/products/jewelry")
-//    public List<JewelryDTO> getAllJewelryDTOs() {
-//        return productService.getAllJewelryDTOs();
-//    }
-    @GetMapping("/products/jewelry/{page}")
-    public List<JewelryDTO> getJewelryDTOs(@PathVariable int page) {
-        return productService.getJewelryDTOs(page);
+    @GetMapping("/products/jewelry")
+    public ResponseEntity<Page<JewelryDTO>> getJewelryByPage(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "8") int size) {
+        Page<JewelryDTO> jewelryPage = jewelryService.getJewelryPage(page, size);
+        if (jewelryPage.hasContent()) {
+            return ResponseEntity.ok(jewelryPage);
+        } else {
+            return ResponseEntity.noContent().build();
+        }
     }
 
     @GetMapping("/products/newrelease/{page}")
--- DiamondShopSystem/src/main/java/com/example/DiamondShopSystem/service/JewelryService.java
+++ DiamondShopSystem/src/main/java/com/example/DiamondShopSystem/service/JewelryService.java
@@ -1,6 +1,7 @@
 package com.example.DiamondShopSystem.service;
 
 import com.example.DiamondShopSystem.dto.AllDataDTO;
+import com.example.DiamondShopSystem.dto.JewelryDTO;
 import com.example.DiamondShopSystem.model.*;
 import com.example.DiamondShopSystem.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -38,6 +39,9 @@ public class JewelryService {
 
     @Autowired
     private ShapeRepository shapeRepository;
+
+    @Autowired
+    private ProductRepository productRepository;
 
     public AllDataDTO getAllData() {
         List<Category> categories = categoryRepository.findAll();
@@ -110,6 +114,11 @@ public class JewelryService {
 //        return jewelryRepository.findAll(pageable);
 //    }
 
+
+    public Page<JewelryDTO> getJewelryPage(int page, int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        return productRepository.findJewelryByPage(pageable);
+    }
 }
 
 
